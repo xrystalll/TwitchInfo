75e3401b64a4eede3950ec1b4e999a77fde5231a
xrystalll <mems.lark@gmail.com>
2020-05-02 16:51:31 +0300

    Updates

---
+++ precache-manifest.a2e0f147b2589f567731e26373a887be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22469ed05455d0a14c82232a87dca0f7",
+    "url": "/TwitchInfo/index.html"
+  },
+  {
+    "revision": "9828d8225670fee14c69",
+    "url": "/TwitchInfo/static/css/main.6c6a11c4.chunk.css"
+  },
+  {
+    "revision": "3e68e4cd2117b84e7865",
+    "url": "/TwitchInfo/static/js/2.e1ed2128.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TwitchInfo/static/js/2.e1ed2128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9828d8225670fee14c69",
+    "url": "/TwitchInfo/static/js/main.3b11d9fe.chunk.js"
+  },
+  {
+    "revision": "11e5281e438ab06b6709",
+    "url": "/TwitchInfo/static/js/runtime-main.874e7abc.js"
+  },
+  {
+    "revision": "a26a488ad9bddb9ac95231aa1bacb9cd",
+    "url": "/TwitchInfo/static/media/twitch-background.a26a488a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06a39f7c40c9d6abb310884caffde544.js"
+  "/TwitchInfo/precache-manifest.a2e0f147b2589f567731e26373a887be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TwitchInfo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
